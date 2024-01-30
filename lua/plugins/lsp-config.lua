f2f6c974c0ce6cc5ff6e24f00dd96db3570de4d4
Yaroslav Rozdobudko <jarekrzdbk@gmail.com>
2024-01-30 18:06:15 +0200

    Added rust lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,13 @@ return {
             lspconfig.perlpls.setup({
                 capabilities = capabilities
             })
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+                on_atttach = on_attach,
+                cmd = {
+                    "rustup", "run", "stable", "rust-analyzer"
+                }
+            })
             vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
