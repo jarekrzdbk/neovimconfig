9a78eab88bcc8c4b8c046aa57e2a1d60364746a8
Yaroslav Rozdobudko <jarekrzdbk@gmail.com>
2024-02-03 18:44:40 +0200

    Comment start treesitter for perl large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,14 +7,14 @@ return {
             ensure_installed = {"lua", "javascript", "c", "ada"},
             highlight = {
                 enable = true,
-                disable = { 'latex' },
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and lang == 'latex' or stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
+                --disable = { 'latex' },
+                --disable = function(lang, buf)
+                --    local max_filesize = 100 * 1024 -- 100 KB
+                --    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                --    if ok and lang == 'latex' or stats and stats.size > max_filesize then
+                --        return true
+                --    end
+                --end,
             },
             indent = { enable = true },
         })
