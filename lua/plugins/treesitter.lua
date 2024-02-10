850a0325e121bd3edae1e933f42dce54b38a657a
Yaroslav Rozdobudko <jarekrzdbk@gmail.com>
2024-02-10 18:17:53 +0200

    Added perl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "javascript", "c", "ada"},
+            ensure_installed = {"lua", "javascript", "c", "ada", "perl"},
             highlight = {
                 enable = true,
                 --disable = { 'latex' },
